50052f2f8e366ce001fbae2ec6c30c19be3a282f
aldanamiranda <aldanamiranda@yahoo.com.ar>
2022-09-22 22:01:40 -0300

    s

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const express = require ('express')
+const express = require("express");
+const path= require('path');
 
-const app = express()
+const app = express();
 
-console.log(app)
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3030, ()=>{
+    console.log ('servidor corriendo en puerto 3030')
+})
+app.use(express.urlencoded({ extended: false }))
+
+app.get ('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'src/views/home.html'))
+})
+app.get ('/register', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'src/views/register.html'))
+})
+
+app.get ('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'src/views/login.html'))
+})
