1f6db630e7092baeace56b1d7e66d274d9f550e8
aldanamiranda <aldanamiranda@yahoo.com.ar>
2022-09-29 18:43:38 -0300

    ,

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path= require('path');
-const mainRouter = require ('..src/routers/main.js')
+const mainRouter = require ('./routers/main.js')
+
+//app.set("view engine", 'ejs')
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use('/', mainRouter);
 
-app.use(express.urlencoded({ extended: false }))
+//app.get('/', (req, res)=>{
+//    res.render('home')
+//})
+
+//app.use(express.urlencoded({ extended: false }))
 
 app.listen(process.env.PORT || 3030, ()=>{
     console.log ('servidor corriendo en puerto 3030')
--- src/controllers/mainControlller.js
+++ src/controllers/mainControlller.js
@@ -4,10 +4,10 @@ const path= require('path');
 
 const mainController = {
     home: (req, res) => {
-            res.sendFile(path.resolve(__dirname, '.src/views/home.html'))
+            res.sendFile(path.resolve(__dirname, '../views/home.html'))
     },
    register: (req,res)=>{
-        res.sendFile(path.resolve(__dirname, '..views/register.html'))
+        res.sendFile(path.resolve(__dirname, '../views/register.html'))
     },
     
     login: (req,res)=>{
