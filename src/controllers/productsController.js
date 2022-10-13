6624a0e14444a8646503a2d38d625a7b35f36759
aldanamiranda <aldanamiranda@yahoo.com.ar>
2022-10-12 23:03:22 -0300

    armo los controladores

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,7 +5,7 @@ const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
-const productsController = {
+const controller = {
 	// Root - Show all products
 	home: (req, res) => {
 		res.render('products', {products})
@@ -27,28 +27,60 @@ const productsController = {
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		console.log(req.body)
-		res.redirect("/")
+
+		let image
+		console.log(req.files);
+		if (req.files[0] != undefined){
+			image= req.files[0].filename
+		}else{
+			image= 'defult-image.png'
+		}
+
+		let newProduct= {
+			id: products[products.length -1].id + 1,
+			image: image,
+			...req.body
+		}
+		products.push(newProduct)
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ''));
+		res.redirect('/')
 		// Do the magic
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		res.render("edit-form")
+		let id= req.params.id;
+		let productToEdit = products.find(producto=>producto.id==id)
+		res.render("edit-form", {productToEdit})
 		// Do the magic
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		console.log(req.body);
-		res.redirect("/")
-		// Do the magic
+		let id= req.params.id
+		let productToEdit= products.find(producto=>producto.id== id)
+
+		productToEdit={
+			id: productToEdit.id,
+			...req.body,
+			image: productToEdit.image,
+		};
+		let newProducts = products.map(producto=>{
+			if (producto.id == productToEdit.id){
+				return producto = {...productToEdit};
+			}
+			return producto;
+		})
+		fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null, ''));
+		res.redirect('/');
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
-		res.redirect("/")
+		let id= req.params.id;
+		let finalProducts = products.filter(product=> product.id != id);
+		fs.writeFileSync(productsFilePath, JSON.stringify(finalProducts, null, ''))
+		res.redirect("/");
 	}
 };
 
-module.exports = productsController;
+module.exports = controller;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,17 @@
-//let userController = {
-//    loginUser: function (req, res){
-//      res.render('login')
-//  }
-// }
+const path =require('path');
+
+
+
+let controller = {
+    register: (req, res)=>{
+        res.render('users/register');
+    },
+    registered: function(req, res){
+
+    },
+    login (req,res) { 
+        res.render('users/login')
+    }
+}
+
+module.exports = controller;
